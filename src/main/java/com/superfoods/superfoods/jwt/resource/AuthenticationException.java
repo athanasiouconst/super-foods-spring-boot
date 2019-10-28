98fcaae5d1b8709d523b155bf265cdaf7a02fcd7
athanasiouconst <csiouath@gmail.com>
2019-10-28 09:42:57 +0200

    authentication service

---
+++ src/main/java/com/superfoods/superfoods/jwt/resource/AuthenticationException.java
@@ -0,0 +1,7 @@
+package com.superfoods.superfoods.jwt.resource;
+public class AuthenticationException extends RuntimeException {
+    public AuthenticationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
+
